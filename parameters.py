7a0631589bee7b301b6310a0a723db94c0d5c5f6
Adyanth-212 <adyanthmallur@gmail.com>
2024-12-04 21:21:07 +0530

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,14 @@
+class Parameters:
+    def __init__(self):
+        self.R = 1  # Radius
+        self.theta = 0
+        self.OM = 0
+        self.MA = 0
+        self.AO = 0
+        self.X = 0
+        self.Y = 0
+        self.Z = 0
+        self.Force = 0
+        self.alpha = 0
+if __name__ == "__main__":
+    params = Parameters()
